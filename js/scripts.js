12212a94ef9f967775e25d77c2f90b2d8f093842
CameronK1990 <cameron.kronsbein@gmail.com>
2021-08-12 13:31:22 -0700

    WIP functions JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
-(document).ready(function() {
-  $("height-form").submit(function(event) {
+$(document).ready(function() {
+  $("#height-form").submit(function(event) {
     event.preventDefault();
     const feet = parseInt($("#input-feet").val()); // $(...) is the jQuery selector for this element: "input#feet" (input html element with the id of feet)
     //.val() gets the user input value in that text input field (the default return value for .val() is a string)
     //parseInt() changes everything inside the parentheses to an integer.
     //then we assign the result to the constant variable "feet"
     const inches= parseInt ($("#input-inches").val());
-    const totalHeight = feet * 12 + inches
+    const totalHeight = feet * 12 + inches;
     
     if (totalHeight >= 72) {
       $("#tall").show();
-      $("everybody").show ();
+      $("#everybody").show ();
     } else if (totalHeight < 63) {
-      $("short").show ();
-      $("everybody").show ();
+      $("#short").show ();
+      $("#everybody").show ();
     } else {
-      $("medium").show();
-      $("everybody").show();
+      $("#medium").show();
+      $("#everybody").show();
     }
   });
-})
+});
