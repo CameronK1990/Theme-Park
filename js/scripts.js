2e828786139e2c7ad66c0533fad6e645bdd6da04
CameronK1990 <cameron.kronsbein@gmail.com>
2021-08-12 11:22:53 -0700

    WIP height form input JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-
+(document).ready(function() {
+  $("height-form").submit(function(event) {
+    event.preventDefault();
+    const feet = parseInt($("#input-feet").val()); // $(...) is the jQuery selector for this element: "input#feet" (input html element with the id of feet)
+    //.val() gets the user input value in that text input field (the default return value for .val() is a string)
+    //parseInt() changes everything inside the parentheses to an integer.
+    //then we assign the result to the constant variable "feet"
+    const inches= parseInt ($("#inches-input").val());
+  }
+}
